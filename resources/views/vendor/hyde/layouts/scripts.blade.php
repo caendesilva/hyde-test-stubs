1b6c65ad0895c72223df52562b50bc02b82a8288
Caen De Silva <caen@desilva.se>
2022-05-05 20:39:44 +0200

    Update framework to 0.23.x

--- resources/views/vendor/hyde/layouts/scripts.blade.php
+++ resources/views/vendor/hyde/layouts/scripts.blade.php
@@ -1,5 +1,4 @@
 {{-- Load scripts from local HydeFront repo: --}}
-{{-- <script defer src="{{ Hyde::relativeLink('../../dist/hyde.js', $currentPage) }}"></script> --}}
-
+<script defer src="{{ Hyde::relativeLink('../../dist/hyde.js', $currentPage) }}"></script>
 {{-- Load latest scripts from CDN --}}
-<script defer src="https://cdn.jsdelivr.net/gh/hydephp/hydefront@master/dist/hyde.js"></script>
+{{-- <script defer src="https://cdn.jsdelivr.net/gh/hydephp/hydefront@master/dist/hyde.js"></script> --}}
--- resources/views/vendor/hyde/layouts/styles.blade.php
+++ resources/views/vendor/hyde/layouts/styles.blade.php
@@ -1,7 +1,7 @@
 {{-- Load scripts from local HydeFront repo: --}}
-{{-- <link rel="stylesheet" href="{{ Hyde::relativeLink('../../dist/hyde.css', $currentPage) }}"> --}}
+<link rel="stylesheet" href="{{ Hyde::relativeLink('../../dist/hyde.css', $currentPage) }}">
 {{-- Load latest styles from CDN --}}
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/hydephp/hydefront@master/dist/hyde.css">
+{{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/hydephp/hydefront@master/dist/hyde.css"> --}}
 
 {{-- The compiled Tailwind/App styles --}}
 @if(Hyde::assetManager()->hasMediaFile('app.css'))
