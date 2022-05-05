71f3b8e3550a34ecdf83ee1da136729f6ace4a9d
Caen De Silva <caen@desilva.se>
2022-05-05 14:54:25 +0200

    Update asset loading

--- resources/views/vendor/hyde/layouts/scripts.blade.php
+++ resources/views/vendor/hyde/layouts/scripts.blade.php
@@ -1,1 +1,5 @@
-<script defer src="{{ Hyde::relativeLink('../../dist/hyde.js', $currentPage) }}"></script>
+{{-- Load scripts from local HydeFront repo: --}}
+{{-- <script defer src="{{ Hyde::relativeLink('../../dist/hyde.js', $currentPage) }}"></script> --}}
+
+{{-- Load latest scripts from CDN --}}
+<script defer src="https://cdn.jsdelivr.net/gh/hydephp/hydefront@master/dist/hyde.js"></script>
